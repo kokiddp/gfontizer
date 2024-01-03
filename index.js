9530d32a3451d05acf5605a394de6822b4b8dc31
Gabriele Coquillard <gabriele.coquillard@gmail.com>
2024-01-03 16:19:52 +0100

    1.1.1

--- index.js
+++ index.js
@@ -5,34 +5,51 @@ const axios = require('axios');
 const path = require('path');
 
 const cssUrl = process.argv[2] || null;
-const cssOutputPath = process.argv[3] || './resources/assets/styles/common/_gfonts.scss';
+const destinationFolder = process.argv[3] || './resources/assets/fonts/';
+const cssOutputPath = process.argv[4] || './resources/assets/styles/common/_gfonts.scss';
 
-const downloadFontsAndCreateStylesheet = async (url, cssOutputPath = './resources/assets/styles/common/_gfonts.scss') => {
+const downloadFontsAndCreateStylesheet = async (url, destinationFolder = './resources/assets/fonts/', cssOutputPath = './resources/assets/styles/common/_gfonts.scss') => {
   try {
+    // Assicurati che la cartella di destinazione esista
+    fs.mkdirSync(destinationFolder, { recursive: true });
+
     // Scarica il file CSS per i .woff2
     const responseWOFF2 = await axios.get(url, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36', // User Agent per un moderno browser Chrome
       }
     });
 
-    let combinedCssText = responseWOFF2.data.replace(/\/\*[\s\S]*?\*\//g, '');
+    // Rimuove i commenti
+    combinedCssText = responseWOFF2.data.replace(/\/\*[\s\S]*?\*\//g, '');
 
     // Trova tutti gli URL dei file dei font
     const fontUrls = combinedCssText.match(/url\((https:\/\/fonts\.gstatic\.com[^)]+)\)/g);
 
-    // Itera gli URL e codifica ciascun file in base64
+    // Itera gli URL e scarica ciascun file
     for (const fontUrl of fontUrls) {
       const actualUrl = fontUrl.slice(4, -1); // Rimuove "url(" e ")"
+      const filename = path.basename(actualUrl);
+      const localPath = path.join(destinationFolder, filename);
       const { data } = await axios.get(actualUrl, { responseType: 'arraybuffer' });
-      const base64Font = Buffer.from(data).toString('base64');
-      const fontFormat = path.extname(actualUrl).substring(1); // Estrae il formato del font dall'URL
-      combinedCssText = combinedCssText.replace(actualUrl, `data:font/${fontFormat};charset=utf-8;base64,${base64Font}`);
+      fs.writeFileSync(localPath, data);
+      console.log(`Downloaded ${filename}`);
+      // Aggiorna l'URL nel CSS
+      const relativePath = path.relative(path.dirname(cssOutputPath), localPath);
+      combinedCssText = combinedCssText.replace(actualUrl, relativePath);
     }
 
+    // Sostituisci tutti gli apici semplici con doppi apici
     combinedCssText = combinedCssText.replace(/'/g, '"');
-    combinedCssText = combinedCssText.replace(/\n{2,}/g, '\n');
+
+    // Sostituisci tutti i doppi ritorni a capo con un singolo ritorno a capo
+    combinedCssText = combinedCssText.trim().replace(/\n{2,}/g, '\n');
+
+    // Aggiungi un ritorno a capo prima di ogni @font-face tranne il primo
     combinedCssText = combinedCssText.split('@font-face').join('\n@font-face').replace('\n', '');
+
+    // Aggiungi una riga vuota alla fine del file
+    combinedCssText = combinedCssText + '\n';
 
     // Assicurati che la cartella di destinazione per il file CSS esista
     fs.mkdirSync(path.dirname(cssOutputPath), { recursive: true });
@@ -45,4 +62,4 @@ const downloadFontsAndCreateStylesheet = async (url, cssOutputPath = './resource
   }
 };
 
-downloadFontsAndCreateStylesheet(cssUrl, cssOutputPath);
+downloadFontsAndCreateStylesheet(cssUrl, destinationFolder, cssOutputPath);
